6e6886fea82dd4fc5369a32db95318c60574a326
Stephen Gichia <stephengichia85@gmail.com>
2026-03-17 16:01:12 +0300

    feat: initial commit

---
+++ constants/index.ts
@@ -0,0 +1,74 @@
+
+export const subjects = [
+  "financial_literacy",
+  "investing",
+  "accounting",
+  "banking",
+  "cryptocurrency",
+  "entrepreneurship",
+];
+
+export const subjectsColors = {
+  financial_literacy: "#E8F7FF",
+  investing: "#D1FFE0",
+  accounting: "#FFF4CC",
+  banking: "#E5D0FF",
+  cryptocurrency: "#FFD6E7",
+  entrepreneurship: "#FFE4C8",
+};
+
+export const voices = {
+  male: { casual: "2BJW5coyhAzSr8STdHbE", formal: "c6SfcYrb2t09NHXiT80T" },
+  female: { casual: "ZIlrSGI4jZqobxRKprJz", formal: "sarah" },
+};
+
+export const recentSessions = [
+  {
+    id: "1",
+    subject: "financial_literacy",
+    name: "Finley the Money Guide",
+    topic: "Budgeting & Expense Tracking",
+    duration: 30,
+    color: "#E8F7FF",
+  },
+  {
+    id: "2",
+    subject: "investing",
+    name: "Investor Ace",
+    topic: "Stocks vs Bonds Explained",
+    duration: 45,
+    color: "#D1FFE0",
+  },
+  {
+    id: "3",
+    subject: "accounting",
+    name: "Ledger Pro",
+    topic: "Understanding Financial Statements",
+    duration: 40,
+    color: "#FFF4CC",
+  },
+  {
+    id: "4",
+    subject: "banking",
+    name: "Banko the Finance Buddy",
+    topic: "How Banks Work & Interest Rates",
+    duration: 25,
+    color: "#E5D0FF",
+  },
+  {
+    id: "5",
+    subject: "cryptocurrency",
+    name: "Crypto Nova",
+    topic: "Blockchain & Digital Assets Basics",
+    duration: 35,
+    color: "#FFD6E7",
+  },
+  {
+    id: "6",
+    subject: "entrepreneurship",
+    name: "BizMaster",
+    topic: "Starting & Scaling a Business",
+    duration: 50,
+    color: "#FFE4C8",
+  },
+];
